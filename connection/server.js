72cfdb2fb01cd93dededdbc046fb5403a202949a
Jerome <raoju1580@qq.com>
2018-11-17 23:42:43 +0800

    长连接

--- connection/server.js
+++ connection/server.js
@@ -2,14 +2,22 @@ const http = require('http')
 const fs = require('fs')
 
 http.createServer((request, response) => {
-    // 基础设置 cookie
+
+    const img = fs.readFileSync('test.jpg')
+
     if(request.url === '/'){        
         const html = fs.readFileSync('test.html','utf8')
         response.writeHead(200,{
             'Content-type':'text/html',
-            'Set-Cookie':['id=123;max-age=10','abc=456;HttpOnly']
+            'Connection':'close'  // 关闭长连接 keep-alive
         })
         response.end(html)
+    }else{
+        response.writeHead(200,{
+            'Content-type':'image/jpg',
+            'Connection':'close'    // 关闭长连接 keep-alive
+        })
+        response.end(img)
     }
 
     
